1e657d03ecfd6b3b2a475dbc0ba2b59c12547697
CC <ccworld1000@gmail.com>
2018-12-18 13:01:39 +0800

    CCAutoTag push : ignore

---
+++ CCTips/CCTips/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCTips
+//
+//  Created by dengyouhua on 2018/12/18.
+//  Copyright © 2018 cc | ccworld1000@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
